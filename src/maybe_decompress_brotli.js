a49a563a8b2ea7050045e3b83901fd64f7690283
Petr Patek <petrpatekml@gmail.com>
2019-06-20 12:57:47 +0200

    improved brotli logic.

--- src/maybe_decompress_brotli.js
+++ src/maybe_decompress_brotli.js
@@ -8,18 +8,22 @@
  */
 module.exports = function maybeDecompressBrotli(response, useBrotli) {
     const nodeVersion = Number(process.version.match(/^v(\d+\.\d+)/)[1]);
-
     const compression = response.headers['content-encoding'];
 
-    if (compression !== 'br' || !useBrotli || nodeVersion >= 12) {
+    if (compression !== 'br' || !useBrotli) {
         return response;
     }
 
     let decompressFunction;
-    try {
+
+    if (nodeVersion >= 12) {
+        decompressFunction = require('zlib').createBrotliDecompress; // eslint-disable-line
+    } else {
+        try {
             decompressFunction = require('iltorb').decompressStream; // eslint-disable-line
-    } catch (e) {
-        throw new Error('You must have iltorb peer dependency installed to use brotli decompression');
+        } catch (e) {
+            throw new Error('You must have iltorb peer dependency installed to use brotli decompression or use NodeJs v12+');
+        }
     }
 
     return response.pipe(decompressFunction());
