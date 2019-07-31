d30188c3ea92077ee0f10f5dfa8af295fb93505d
Petr Patek <petrpatekml@gmail.com>
2019-07-31 13:17:20 +0200

    fixed headers deduplication. modified docs. added Apify eslint

--- src/monkey_patch_headers.js
+++ src/monkey_patch_headers.js
@@ -32,7 +32,7 @@ function monkeyPatchHeaders(options) {
         if (foundHeader) {
             return foundHeader;
         }
-        return name.toLowerCase();
+        return name;
     };
 
     // Node.Js V8 handles the headers in a different way.
@@ -59,15 +59,15 @@ function monkeyPatchHeaders(options) {
         this[outHeadersKey][key] = [key, value];
 
             switch (key.length) { //eslint-disable-line
-        case 10:
-            if (key === 'connection') this._removedConnection = false;
-            break;
-        case 14:
-            if (key === 'content-length') this._removedContLen = false;
-            break;
-        case 17:
-            if (key === 'transfer-encoding') this._removedTE = false;
-            break;
+            case 10:
+                if (key === 'connection') this._removedConnection = false;
+                break;
+            case 14:
+                if (key === 'content-length') this._removedContLen = false;
+                break;
+            case 17:
+                if (key === 'transfer-encoding') this._removedTE = false;
+                break;
         }
     };
 }
